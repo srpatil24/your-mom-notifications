77b8c3133c4e8a8a08a601a096d9b9a1e6d102aa
Sid <siddhant.rahulpatil@gmail.com>
2024-11-09 21:04:34 -0600

    push pull conflicts resolution

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,4 @@
 import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 import Colors from '@/constants/Colors';
